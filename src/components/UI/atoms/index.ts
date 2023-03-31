3bca56ec666621815c4ed94ad7f541c368b46138
Enalpe <2236561@cognizant.com>
2023-03-31 17:17:20 +0800

    first commit

---
+++ src/components/UI/atoms/index.ts
@@ -0,0 +1,8 @@
+export { default as Box } from "./Box/Box";
+export { default as Burger } from "./Burger/Burger";
+export { default as Button } from "./Button/Button";
+export { default as Button } from "./Container/Container";
+export { default as Button } from "./Modal/Modal";
+export { default as Button } from "./Navbar/Navbar";
+export { default as Button } from "./Papers/Papers";
+export { default as Button } from "./Table/Table";
