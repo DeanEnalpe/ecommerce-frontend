c9d0b9bceb65b6ca652f43fee2a62a72da6e94b9
Mataac <2236558@cognizant.com>
2023-04-03 14:27:46 +0800

    added login page

--- src/components/templates/Login/Login.tsx
+++ src/components/templates/Login/Login.tsx
@@ -1,7 +1,121 @@
-import React from "react";
+import { useToggle, upperFirst } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
+import {
+  TextInput,
+  PasswordInput,
+  Text,
+  Paper,
+  Group,
+  PaperProps,
+  Button,
+  Divider,
+  Checkbox,
+  Anchor,
+  Stack,
+} from "@mantine/core";
+// import { GoogleButton, TwitterButton } from '../SocialButtons/SocialButtons';
 
-const Login = () => {
-  return <div>Login</div>;
-};
+export default function AuthenticationForm(props: PaperProps) {
+  const [type, toggle] = useToggle(["login", "register"]);
+  const form = useForm({
+    initialValues: {
+      email: "",
+      name: "",
+      password: "",
+      terms: true,
+    },
 
-export default Login;
+    validate: {
+      email: (val) => (/^\S+@\S+$/.test(val) ? null : "Invalid email"),
+      password: (val) =>
+        val.length <= 6
+          ? "Password should include at least 6 characters"
+          : null,
+    },
+  });
+
+  return (
+    <Paper radius="md" p="xl" withBorder {...props}>
+      <Text size="lg" weight={500}>
+        Welcome to ECommerce System, {type} with
+      </Text>
+
+      {/* <Group grow mb="md" mt="md">
+        <GoogleButton radius="xl">Google</GoogleButton>
+        <TwitterButton radius="xl">Twitter</TwitterButton>
+      </Group> */}
+
+      <Divider label="Or continue with email" labelPosition="center" my="lg" />
+
+      <form onSubmit={form.onSubmit(() => {})}>
+        <Stack>
+          {type === "register" && (
+            <TextInput
+              label="Name"
+              placeholder="Your name"
+              value={form.values.name}
+              onChange={(event: { currentTarget: { value: string } }) =>
+                form.setFieldValue("name", event.currentTarget.value)
+              }
+              radius="md"
+            />
+          )}
+
+          <TextInput
+            required
+            label="Email"
+            placeholder="ecommerce@gmail.com"
+            value={form.values.email}
+            onChange={(event: { currentTarget: { value: string } }) =>
+              form.setFieldValue("email", event.currentTarget.value)
+            }
+            error={form.errors.email && "Invalid email"}
+            radius="md"
+          />
+
+          <PasswordInput
+            required
+            label="Password"
+            placeholder="Your password"
+            value={form.values.password}
+            onChange={(event: { currentTarget: { value: string } }) =>
+              form.setFieldValue("password", event.currentTarget.value)
+            }
+            error={
+              form.errors.password &&
+              "Password should include at least 6 characters"
+            }
+            radius="md"
+          />
+
+          {type === "register" && (
+            <Checkbox
+              label="I accept terms and conditions"
+              checked={form.values.terms}
+              onChange={(event: { currentTarget: { checked: boolean } }) =>
+                form.setFieldValue("terms", event.currentTarget.checked)
+              }
+            />
+          )}
+        </Stack>
+
+        <Group position="apart" mt="xl">
+          <Anchor
+            component="button"
+            type="button"
+            color="dimmed"
+            onClick={() => toggle()}
+            size="xs"
+          >
+            {type === "register"
+              ? "Already have an account? Login"
+              : "Don't have an account? Register"}
+          </Anchor>
+          <Button type="submit" radius="xl">
+            {upperFirst(type)}
+          </Button>
+        </Group>
+      </form>
+    </Paper>
+  );
+}
